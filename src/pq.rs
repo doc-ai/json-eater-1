ea6339472d8df9fa6849459f811699835c3e0d48
Kartik Thakore <kartik@doc.ai>
2021-04-15 15:31:57 -0700

    Making write_pq

--- src/pq.rs
+++ src/pq.rs
@@ -335,17 +335,78 @@ fn pq_eat(data: &str,
         Some(is_records) => is_records,
     };
 
-    let headers: Value = Value::Null;
-
     let mut _data: HashMap<String, Vec<Value>> = HashMap::new();
 
-
-    let headers = generate_headers(&value, header_paths);
     let mut sample_obj = Sample::default().to_value();
+    let headers = generate_headers(&value, header_paths.clone());
     merge_headers(&mut sample_obj, &headers);
     let sample_schema = Schema::from_value(sample_obj);
 
-    deep_write(sample_schema.clone(), &value, current_path, headers, &mut _data);
+
+match target_json_path {
+        None => {
+            if is_records && value.is_array() {
+                // Make sure value is an array
+                for (_i, v) in value.as_array().unwrap().iter().enumerate() {
+                    
+                    let headers = generate_headers(&v, header_paths.clone());
+                    let current_path = vec![String::from("$root")];
+                  
+                    deep_write(sample_schema.clone(), v, current_path, headers, &mut _data);
+                
+                }
+                // Iterate through
+            }
+
+            deep_write(sample_schema.clone(), &value, current_path, headers, &mut _data);
+        }
+        Some(path) => {
+            if is_records && value.is_array() {
+                // Make sure value is an array
+
+                for (_i, v) in value.as_array().unwrap().iter().enumerate() {
+                    
+                    let headers = generate_headers(&v, header_paths.clone());
+                    let target_value = v.pointer(path.as_str());
+                    match target_value {
+                        Some(target_value) => {
+                            let current_path = vec![String::from("$root")];
+
+                             deep_write(sample_schema.clone(), &target_value, current_path, headers, &mut _data);
+                        }
+                        None => {
+                            println!("No path found for[{}] {:?}", _i, path.as_str());
+                            ()
+                        }
+                    }
+                }
+            // Iterate through
+            } else {
+                let target_value = value.pointer(path.as_str()).unwrap();
+
+                match is_str_json {
+                    None => {
+                        
+                        deep_write(sample_schema.clone(), &target_value, current_path, headers, &mut _data);
+                    }
+                    Some(is_str_json) => {
+                        // If true we have to reparse this string
+                        if is_str_json {
+                            let v = serde_json::from_str(target_value.as_str().unwrap())
+                                .expect("Invalid json_pointer target is not a json string");
+
+                            
+                                deep_write(sample_schema.clone(), &v, current_path, headers, &mut _data);
+                        } else {
+                            
+                            deep_write(sample_schema.clone(), &target_value, current_path, headers, &mut _data);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 
 
     let loc: &str = match loc {
